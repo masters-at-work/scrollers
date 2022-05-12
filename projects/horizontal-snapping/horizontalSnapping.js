6403e949e66c50d3d2a8adcda611c57079188bc0
Ray Weitzenberg <ray.weitzenberg@uniqlo-usa.com>
2022-05-12 12:02:27 -0400

    reduced sensitivity by increasing the 'end' value

--- projects/horizontal-snapping/horizontalSnapping.js
+++ projects/horizontal-snapping/horizontalSnapping.js
@@ -9,16 +9,16 @@ gsap.to(sections, {
         trigger: ".container",
         pin: true,
         scrub: 1,
+        // horizontal: true,x
+        // start: "left center",
         // snap documentation on greensock.com
         // https://greensock.com/docs/v3/Plugins/ScrollTrigger#:~:text=smoothing%20things%20out.-,snap,-Number%20%7C%20Array%20%7C%20Function
         snap: {
-          snapTo: 1 / (sections.length - 1),
-            duration: 0.2, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-            delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+            snapTo: 1 / (sections.length - 1),
+            duration: 0.2,
+            delay: 0, // wait 0.2 seconds from the last scroll event before doing the snapping
             ease: "power1.inOut", // the ease of the snap animation ("power3" by default)
         },
-        // start: 500,
-        // base vertical scrolling on how wide the container is so it feels more natural.
-        end: "+=1000",
+        end: "+=2500", // increased this to make the scrolling less sensitive
     },
 });
